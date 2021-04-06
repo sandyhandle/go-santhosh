83202d8b4480ed7b7793434e447ead277b4038ec
sandyhandle <64911148+sandyhandle@users.noreply.github.com>
2021-04-06 11:50:29 +0530

    Arthmetic operator (#7)
    * the arthmetic operator file is created.
    
    * arthmetic operator module is created and used successfully..:-)

--- Arthmetic_Operator.go
+++ Arthmetic_Operator.go
@@ -1,1 +1,15 @@
 // good to go
+package main
+
+import (
+	"fmt"
+	// we can also use "math" module
+)
+
+func main() {
+	var num1 int = 9
+	var num2 int = 5
+	var num3 float64 = float64(num1) / float64(num2)
+	fmt.Printf("The output will be: %f", num3)
+
+}
