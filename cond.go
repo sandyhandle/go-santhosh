03fe2236e2d02ec4c9fffe2bbbb87d6b4a799173
sandyhandle <64911148+sandyhandle@users.noreply.github.com>
2021-04-06 12:26:13 +0530

    Temp new (#9)
    * mode some changes in the conditional statements.
    
    * In the string comparisation some changes were added.

--- cond.go
+++ cond.go
@@ -4,8 +4,10 @@ import "fmt"
 
 func main() {
 	fmt.Println("Numerical Comparisation")
+
 	x := 1
 	var y int = 2
+
 	fmt.Printf("%t \n", x < y)
 	fmt.Printf("%t \n", x <= y)
 	fmt.Printf("%t \n", x > y)
@@ -14,6 +16,7 @@ func main() {
 	fmt.Printf("%t\n", x != y)
 
 	fmt.Println("String Comparisation")
-
-	fmt.Printf("%t\n", "Hello" == "hello")
+	var s string = "hello"
+	fmt.Printf("%t\n", "Hello" == s)
+	fmt.Printf("%t\t,%t\t,%t\t,%t", 'B' < 'a', 'B' <= 'C', 'G' > 'L', 'Z' >= 'Y')
 }
