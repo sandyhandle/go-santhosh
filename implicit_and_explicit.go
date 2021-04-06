a2f11b5274b481d3b62d022249260c95aab082a7
Santhosh Puvaneswaran <santhosh.puvaneswaran@gmail.com>
2021-04-06 09:32:41 +0530

    Assignment explanation & Implicit and Explicit. A file is created and practice is done

---
+++ implicit_and_explicit.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var num float64
+	number := 88
+	var bl bool
+	fmt.Println("num = ", num)
+	fmt.Println("boolen =", bl)
+	fmt.Println("number =", number)
+	fmt.Printf("%T", num)
+
+	fmt.Println()
+	fmt.Printf("%T", bl)
+	fmt.Println()
+	fmt.Printf("%T", number)
+}
